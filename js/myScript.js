81733860f558a8ef156ddebf8b6580091fcbe8c5
Ritwik <95333659+Ritwik291100@users.noreply.github.com>
2023-05-27 20:19:23 +0530

    Delete js directory

--- js/myScript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#itmname").keyup(function(){
-        $.get("../Source/item.php", {iname: $(this).val()}, function(data){
-        	$("datalist").empty();
-        	$("datalist").html(data);
-        });
-    });
